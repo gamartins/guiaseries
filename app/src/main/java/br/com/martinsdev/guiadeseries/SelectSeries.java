6b7a24b91ff423215d9a2f03512972ee5c9a8d19
Gabriel Angelo <gabriel.angelo91@gmail.com>
2015-12-03 11:05:10 -0300

    Projeto criado no Android Studio

---
+++ app/src/main/java/br/com/martinsdev/guiadeseries/SelectSeries.java
@@ -0,0 +1,13 @@
+package br.com.martinsdev.guiadeseries;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectSeries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_series);
+    }
+}
